f213bc0708074f7992055e42ae21b1e7cb1d9963
lisvalver <lisvalver.atte@gmail.com>
2020-09-28 20:45:59 -0300

    exercise 2

--- exercises/7_2/index.js
+++ exercises/7_2/index.js
@@ -1,27 +1,33 @@
 const lesson1 = {
-    materia: 'Matemática',
-    numeroEstudantes: 20,
-    professor: 'Maria Clara',
-    turno: 'manhã',
-  };
-  
-  const lesson2 = {
-    materia: 'História',
-    numeroEstudantes: 20,
-    professor: 'Carlos',
-  };
-  
-  const lesson3 = {
-    materia: 'Matemática',
-    numeroEstudantes: 10,
-    professor: 'Maria Clara',
-    turno: 'noite',
-  };
+  materia: "Matemática",
+  numeroEstudantes: 20,
+  professor: "Maria Clara",
+  turno: "manhã",
+};
 
-  const addTurn = (object, key, value) => {
-      object[key] = value;
-  };
+const lesson2 = {
+  materia: "História",
+  numeroEstudantes: 20,
+  professor: "Carlos",
+};
 
-  addTurn(lesson2, 'turno', 'manhã');
+const lesson3 = {
+  materia: "Matemática",
+  numeroEstudantes: 10,
+  professor: "Maria Clara",
+  turno: "noite",
+};
 
-  console.log(lesson2);
+//   exercise 1
+
+const addTurn = (object, key, value) => object[key] = value;
+
+addTurn(lesson2, "turno", "manhã");
+
+console.log(lesson2);
+
+// exercise 2
+
+const listKeys = (object) => console.log(Object.keys(object));
+
+console.log(listKeys(lesson3));
