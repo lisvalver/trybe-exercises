9e091b2aa74ddc9cc05f6751fc5b470a6f03b300
lisvalver <lisvalver.atte@gmail.com>
2020-10-15 19:41:01 -0300

    exercise 1

--- exercises/8_3/exercise1.js
+++ exercises/8_3/exercise1.js
@@ -8,7 +8,7 @@ const arrays = [
 
 
 function flatten() {
-  // escreva seu código aqui
-}
+  return arrays.reduce((acc, next) => acc.concat(next));
+};
 
 assert.deepEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
